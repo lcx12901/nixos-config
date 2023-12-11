5e7e0ecf4848824a92384ea29f24576f3f8b10ea
lcx12901 <wktl1991504424@gmail.com>
2023-12-11 22:54:41 +0800

    update

---
+++ flake.nix
@@ -0,0 +1,101 @@
+#           ▜███▙       ▜███▙  ▟███▛
+#            ▜███▙       ▜███▙▟███▛
+#             ▜███▙       ▜██████▛
+#      ▟█████████████████▙ ▜████▛     ▟▙
+#     ▟███████████████████▙ ▜███▙    ▟██▙
+#            ▄▄▄▄▖           ▜███▙  ▟███▛
+#           ▟███▛             ▜██▛ ▟███▛
+#          ▟███▛               ▜▛ ▟███▛
+# ▟███████████▛                  ▟██████████▙
+# ▜██████████▛                  ▟███████████▛
+#       ▟███▛ ▟▙               ▟███▛
+#      ▟███▛ ▟██▙             ▟███▛
+#     ▟███▛  ▜███▙           ▝▀▀▀▀
+#     ▜██▛    ▜███▙ ▜██████████████████▛
+#      ▜▛     ▟████▙ ▜████████████████▛
+#            ▟██████▙       ▜███▙
+#           ▟███▛▜███▙       ▜███▙
+#          ▟███▛  ▜███▙       ▜███▙
+#          ▝▀▀▀    ▀▀▀▀▘       ▀▀▀▘
+# » Implemented machines:
+#    • wsl → WSL on the daily driver.
+
+{
+  description = "A tough try on NixOS";
+
+  outputs = input @ { self, nixpkgs, home-manager, nixos-wsl, nix-ld, ... }:
+    let
+      username = "wktl";
+      userfullname = "wktl Lin";
+      useremail = "wktl1991504424@gmail.com";
+
+      system = "x86_64-linux";
+      allSystems = [x64_system];
+
+      nixosSystem = import ./lib/nixosSystem.nix;
+
+      Mikasa_modules = {
+        nixos-modules = [
+          nixos-wsl.nixosModules.wsl
+          ./hosts/Mikasa
+        ];
+        home-module = import ./home/wktl/Mikasa.nix;
+      };
+
+      x64_specialArgs =
+        {
+          inherit username userfullname useremail;
+          pkgs = import nixpkgs {
+            inherit system; # refer the `system` parameter form outer scope recursively
+            # To use chrome, we need to allow the installation of non-free software
+            config.allowUnfree = true;
+          };
+        }
+        // inputs;
+    in
+    {
+      nixosConfigurations = let
+        base_args = {
+          inherit home-manager;
+          nixpkgs = nixpkgs;
+          system = x64_system;
+          specialArgs = x64_specialArgs;
+        };
+      in {
+        Mikasa = nixosSystem (Mikasa_modules // base_args);
+      };
+
+      # format the nix code in this flake
+      # alejandra is a nix formatter with a beautiful output
+      formatter = nixpkgs.lib.genAttrs allSystems (
+        system:
+          nixpkgs.legacyPackages.${system}.alejandra
+      );
+    };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nur.url = "github:nix-community/NUR";
+
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-ld = {
+      url = "github:Mic92/nix-ld";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
---
+++ hosts/Mikasa/default.nix
@@ -0,0 +1,73 @@
+{...} @ args:
+{
+
+  time.timeZone = "Asia/Shanghai";
+
+  networking.hostName = "Mikasa";
+
+  systemd.tmpfiles.rules = [
+    "d /home/${username}/.config 0755 ${username} users"
+  ];
+
+  environment.enableAllTerminfo = true;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services.openssh.enable = true;
+
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+    ];
+  };
+
+  wsl = {
+    enable = true;
+    wslConf.automount.root = "/mnt";
+    wslConf.interop.appendWindowsPath = false;
+    wslConf.network.generateHosts = false;
+    defaultUser = username;
+    startMenuLaunchers = true;
+
+    # Enable integration with Docker Desktop (needs to be installed)
+    docker-desktop.enable = false;
+  };
+
+  nix = {
+    settings = {
+      trusted-users = [username];
+      # FIXME: use your access tokens from secrets.json here to be able to clone private repos on GitHub and GitLab
+      # access-tokens = [
+      #   "github.com=${secrets.github_token}"
+      #   "gitlab.com=OAuth2:${secrets.gitlab_token}"
+      # ];
+
+      accept-flake-config = true;
+      auto-optimise-store = true;
+    };
+
+    registry = {
+      nixpkgs = {
+        flake = inputs.nixpkgs;
+      };
+    };
+
+    nixPath = [
+      "nixpkgs=${inputs.nixpkgs.outPath}"
+      "nixos-config=/etc/nixos/configuration.nix"
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
+
+    package = pkgs.nixFlakes;
+    extraOptions = ''experimental-features = nix-command flakes'';
+
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 7d";
+    };
+  };
+
+
+  system.stateVersion = "23.05";
+}
