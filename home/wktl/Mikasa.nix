b960252d47feea7e393b8f19aca2e67c6cead593
lcx12901 <wktl1991504424@gmail.com>
2023-12-11 23:13:56 +0800

    update

--- home/wktl/Mikasa.nix
+++ home/wktl/Mikasa.nix
@@ -1,3 +1,34 @@
-{
+{ username, useremail, nix-index-database, pkgs, ... }: {
+  home = {
+    username = username;
+    homeDirectory = "/home/${username}";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "23.05";
+  };
+
+  home.packages = with pkgs; [
+    wget
+  ];
+
+  programs = {
+    home-manager.enable = true;
+    nix-index.enable = true;
+    nix-index.enableZshIntegration = true;
+    nix-index-database.comma.enable = true;
+
+    git = {
+      enable = true;
+      userEmail = useremail; # FIXME: set your git email
+      userName = username; #FIXME: set your git username
+    };
+  };
   
 }
