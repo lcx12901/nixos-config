4d7e73d4308b83bb2a90e34ab99c5c904378fb37
lcx12901 <wktl1991504424@gmail.com>
2023-12-12 23:29:29 +0800

    update

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -2,14 +2,12 @@
   nix = {
     settings = {
       trusted-users = ["root" "@wheel"];
-
       accept-flake-config = true;
       auto-optimise-store = true;
+      warn-dirty = false;
     };
-    warn-dirty = false;
     package = pkgs.nixFlakes;
     extraOptions = ''experimental-features = nix-command flakes'';
-
     gc = {
       automatic = true;
       dates = "weekly";
