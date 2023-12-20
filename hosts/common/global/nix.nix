81e0baa3abedd5fc8a30ea998c484500d2b48e5b
lcx12901 <wktl1991504424@gmail.com>
2023-12-20 12:02:36 +0800

    update

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -3,9 +3,11 @@
     settings = {
       trusted-users = ["root" "@wheel"];
       accept-flake-config = true;
+      # 使 nix 在每次构建（对于构建路径）后优化存储
       auto-optimise-store = true;
       warn-dirty = false;
     };
+    # nix.optimise.automatic 设置一个 systemd 计时器，定期检查所有路径并优化它们。默认情况下，每天发生一次，可以自定义配置计划
     optimise.automatic = true;
     package = pkgs.nixFlakes;
     extraOptions = ''experimental-features = nix-command flakes'';
