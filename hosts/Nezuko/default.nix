c95317d35b293661c9b78d5bc48cd84a2ac2e93d
lcx12901 <wktl1991504424@gmail.com>
2023-12-31 18:30:57 +0800

    update

--- hosts/Nezuko/default.nix
+++ hosts/Nezuko/default.nix
@@ -3,7 +3,10 @@
     ./hardware-configuration.nix
 
     ../common/global
+    ../common/global/optin-persistence.nix
+
     ../common/users/wktl
+
     ../common/optional/brtfs.nix
     ../common/optional/systemd-boot.nix
   ];
