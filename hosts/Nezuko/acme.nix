1303d667f6a564df9a4c70d54fca31df3f596b29
lcx12901 <wktl1991504424@gmail.com>
2024-01-03 00:03:51 +0800

    update

--- hosts/Nezuko/acme.nix
+++ hosts/Nezuko/acme.nix
@@ -2,8 +2,8 @@
   security.acme = {
     acceptTerms = true;
     defaults.email = "wktl1991504424@gmail.com";
-    certs."nezuko.linx.top" = {
-      domain = "*.nezuko.linx.top";
+    certs."nezuko.lincx.top" = {
+      domain = "*.nezuko.lincx.top";
       dnsProvider = "cloudflare";
       credentialsFile = config.age.secrets."cloudflareGlobalAPIKey.age".path;
     };
