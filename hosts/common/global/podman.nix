5a51d516452828633e4b5c9882ef1aef4098121e
lcx12901 <wktl1991504424@gmail.com>
2024-01-02 14:08:49 +0800

    ddns-go podman

--- hosts/common/global/podman.nix
+++ hosts/common/global/podman.nix
@@ -9,4 +9,14 @@ in
     dockerSocket.enable = !dockerEnabled;
     defaultNetwork.settings.dns_enabled = true;
   };
+
+  virtualisation.oci-containers.backend = "podman";
+
+  virtualisation.oci-containers.containers = {
+    ddns-go = {
+      image = "jeessy/ddns-go:latest";
+      autoStart = true;
+      extraOptions = [ "--network=host" ];
+    };
+  };
 }
