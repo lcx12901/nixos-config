2010fdca42d1aed8c22dbd45d992f2d695af083b
lcx12901 <wktl1991504424@gmail.com>
2024-01-02 23:29:02 +0800

    update

--- hosts/Nezuko/acme.nix
+++ hosts/Nezuko/acme.nix
@@ -4,11 +4,11 @@
     defaults.email = "wktl1991504424@gmail.com";
     certs."nezuko.linx.top" = {
       dnsProvider = "cloudflare";
-      credentialsFile = config.age.secrets."cloudflareGlobalApiKey.age".path
+      credentialsFile = config.age.secrets."cloudflareGlobalApiKey.age".path;
     };
     certs."*.nezuko.linx.top" = {
       dnsProvider = "cloudflare";
-      credentialsFile = config.age.secrets."cloudflareGlobalApiKey.age".path
+      credentialsFile = config.age.secrets."cloudflareGlobalApiKey.age".path;
     };
   };
 }
