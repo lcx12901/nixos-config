98836ab1211a42f79a430f68e96aeaee0ea932f0
lcx12901 <wktl1991504424@gmail.com>
2024-01-03 22:18:19 +0800

    1

--- hosts/common/optional/dae.nix
+++ hosts/common/optional/dae.nix
@@ -2,7 +2,7 @@
   services.dae = {
     enable = true;
     disableTxChecksumIpGeneric = false;
-    configFile = config.age.secrets."dae.config.age".path;
+    configFile = config.age.secrets."config.dae".path;
     assets = with pkgs; [ v2ray-geoip v2ray-domain-list-community ];
   };
 
