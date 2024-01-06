2d7a754864eabab5253dc74d20689a8f8bb4e95f
lcx12901 <wktl1991504424@gmail.com>
2024-01-06 14:17:20 +0800

    1

--- hosts/common/optional/daed.nix
+++ hosts/common/optional/daed.nix
@@ -1,12 +1,11 @@
 { pkgs, daeuniverse, ... }: {
   environment.systemPackages =
   with daeuniverse.packages.x86_64-linux;
-    [ daed ];
-
-  environment.systemPackages = with pkgs; [ 
-    v2ray-geoip 
-    v2ray-domain-list-community 
-  ];
+    [ 
+      daed 
+      pkgs.v2ray-geoip 
+      pkgs.v2ray-domain-list-community 
+    ];
 
   services.daed = {
       enable = true;
