a72221a8e73e38cf2eeefa6f6d09093549f0081d
lcx12901 <wktl1991504424@gmail.com>
2024-01-08 00:04:13 +0800

    1

--- hosts/common/optional/postgresql.nix
+++ hosts/common/optional/postgresql.nix
@@ -7,7 +7,7 @@
     enableTCPIP = true;
     authentication = ''
       #type database DBuser origin-address auth-method
-      host  all      all     127.0.0.1/32   trust
+      host  all      all     192.168.31.0/24   trust
       host all       all     ::1/128        trust
     '';
   };
