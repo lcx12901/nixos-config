0b800485c901d8b5239352129f8ad41a1db86534
lcx12901 <wktl1991504424@gmail.com>
2024-01-07 23:18:38 +0800

    1

--- hosts/Nezuko/website/default.nix
+++ hosts/Nezuko/website/default.nix
@@ -40,16 +40,5 @@
     };
   };
 
-  services.uwsgi = {
-    enable = true;
-    user = "nginx";
-    group = "nginx";
-    plugins = [ "cgi" ];
-    instance = {
-      type = "emperor";
-      vassals = lib.mkBefore { };
-    };
-  };
-
   networking.firewall.allowedTCPPorts = [ 302 404 ];
 }
