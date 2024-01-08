db3a58d3a84a066d216a81c890c6b97170ee3128
lcx12901 <wktl1991504424@gmail.com>
2024-01-08 22:18:11 +0800

    postgre allow ip

--- hosts/common/optional/postgresql.nix
+++ hosts/common/optional/postgresql.nix
@@ -7,6 +7,7 @@
     authentication = ''
       #type database DBuser origin-address auth-method
       host  all      all     192.168.31.0/24   trust
+      host  all      all     10.88.0.1/16   trust
       host all       all     ::1/128        trust
     '';
   };
