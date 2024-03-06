59c5bd943eeec392213dca80512145f1063392dc
lcx12901 <wktl1991504424@gmail.com>
2024-03-06 17:48:51 +0800

    WIP: nvim config

--- home/wktl/features/desktop/neovim/user_config/init.lua
+++ home/wktl/features/desktop/neovim/user_config/init.lua
@@ -52,5 +52,100 @@ return {
       end,
     },
     -- Language Parser for syntax highlighting / indentation / folding / Incremental selection
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        local utils = require("astronvim.utils")
+        opts.incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>", -- Ctrl + Space
+            node_incremental = "<C-space>",
+            scope_incremental = "<A-space>", -- Alt + Space
+            node_decremental = "<bs>", -- Backspace
+          },
+        }
+        opts.ignore_install = { "gotmpl" }
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {
+          -- neovim
+          "vim",
+          "lua",
+          -- operation & cloud native
+          "dockerfile",
+          "hcl",
+          "jsonnet",
+          "regex",
+          "terraform",
+          "nix",
+          "csv",
+          -- other programming language
+          "diff",
+          "gitignore",
+          "gitcommit",
+          "latex",
+          "sql",
+          -- Lisp like
+          "fennel",
+          "clojure",
+          "commonlisp",
+          -- customized languages:
+          "scheme",
+        })
+
+        -- add support for scheme
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.scheme = {
+          install_info = {
+            url = "https://github.com/6cdh/tree-sitter-scheme", -- local path or git repo
+            files = { "src/parser.c" },
+            -- optional entries:
+            branch = "main",                  -- default branch in case of git repo if different from master
+            generate_requires_npm = false,    -- if stand-alone parser without npm dependencies
+            requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+          },
+        }
+        -- use scheme parser for filetypes: scm
+        vim.treesitter.language.register("scheme", "scm")
+      end,
+    },
+    { "Olical/nfnl",                                       ft = "fennel" },
+    -- Lua implementation of CamelCaseMotion, with extra consideration of punctuation.
+    { import = "astrocommunity.motion.nvim-spider" },
+    {
+      "0x00-ketsu/autosave.nvim",
+      -- lazy-loading on events
+      event = { "InsertLeave", "TextChanged" },
+      opts = function(_, opts)
+        opts.prompt_style = "stdout" -- notify or stdout
+      end,
+    },
+    -- File explorer(Custom configs)
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true, -- visible by default
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        },
+      },
+    },
+    -- The plugin offers the alibity to refactor code.
+    {
+      "ThePrimeagen/refactoring.nvim",
+      dependencies = {
+        { "nvim-lua/plenary.nvim" },
+        { "nvim-treesitter/nvim-treesitter" },
+      },
+    },
+    -- The plugin offers the abilibty to search and replace.
+    {
+      "nvim-pack/nvim-spectre",
+      dependencies = {
+        { "nvim-lua/plenary.nvim" },
+      },
+    },
   },
 }
