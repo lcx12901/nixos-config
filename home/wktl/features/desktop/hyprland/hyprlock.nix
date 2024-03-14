8674aaf4cb6eb2f5adb5ddd45900823f899b35b3
lcx12901 <wktl1991504424@gmail.com>
2024-03-14 22:51:23 +0800

    update

--- home/wktl/features/desktop/hyprland/hyprlock.nix
+++ home/wktl/features/desktop/hyprland/hyprlock.nix
@@ -47,7 +47,7 @@
     # TIME
     label {
         monitor =
-        text = cmd[update:1000] echo "$(date +"%-I:%M%p")"
+        text = cmd[update:60000] echo "$(date +"%-I:%M%p")"
         color = rgba(0, 0, 0, 0.5)
         #color = rgba(255, 255, 255, 0.6)
         font_size = 120
