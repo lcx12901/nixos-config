f2cd535eaf45b766b05e4ae3a832ff793ebfdbdb
lcx12901 <wktl1991504424@gmail.com>
2024-03-29 11:52:11 +0800

    update

--- packages/mihomo/default.nix
+++ packages/mihomo/default.nix
@@ -4,7 +4,7 @@
 }:
 buildGoModule rec {
   name = "mihomo-meta";
-  version = "9c08e936f971e4261e854d7bf039e28ab3eb9593";
+  version = "11f0983e5c3e4d3405c2cb1a9f604299e5b1c5fa";
 
   src = fetchFromGitHub {
     owner = "MetaCubeX";
@@ -12,7 +12,7 @@ buildGoModule rec {
     # 对应的 commit 或者 tag，注意 fetchFromGitHub 不能跟随 branch！
     rev = "${version}";
 
-    hash = "sha256-vQfz6vxdu0X6cHk/2vYMs2iBPQ9NitUh3rVIJZMa02I=";
+    hash = "sha256-M3Y5ihua8k+7emZctRWwQxOrH6uUIskRDrl2hziU+D0=";
   };
 
   vendorHash = "sha256-k4xB/jO78VGD+n9HtuoWXoXB+kZCEyPKJTTwj32nGIw=";
@@ -25,7 +25,7 @@ buildGoModule rec {
   ldflags = [
     "-s"
     "-w"
-    "-X github.com/metacubex/mihomo/constant.Version=alpha-9c08e93"
+    "-X github.com/metacubex/mihomo/constant.Version=alpha-11f0983"
   ];
 
   tags = [
